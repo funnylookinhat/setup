298469a0011bba8afd5854f06d9346a56ae3cd40
David Overcash <funnylookinhat@gmail.com>
2021-08-04 15:30:19 -0700

    Update 2021-08-04-15-30

--- base_install.sh
+++ base_install.sh
@@ -20,16 +20,13 @@ sudo apt-get install -y \
   software-properties-common jq \
   postgresql-client mysql-client \
   openconnect libsecret-tools gpa \
-  byobu
+  byobu meld
 
 echo "Configuring git"
 # Optionally run this if you have a GUI
-# git config --global merge.tool meld
+git config --global merge.tool meld
 git config --global user.name "David Overcash"
 git config --global user.email funnylookinhat@gmail.com
-
-# TODO
-# Install files for: saml2js, awscli, tfenv, etc.
 
 for INSTALL_FILE in $(find base_install_files/ -type f); do
   echo "Running $INSTALL_FILE"
