9de98fe64dd4ac14caf19c048e3d5a1039381836
David Overcash <funnylookinhat@gmail.com>
2021-10-31 05:57:17 -0700

    Update 2021-10-31-05-57

--- base_install_files/docker.sh
+++ base_install_files/docker.sh
@@ -11,12 +11,11 @@ fi
 
 sudo apt-get update && sudo apt-get install -y docker.io
 
-# DNS Points to AWS
-# Necessary for run-remote scripts
-echo '{"insecure-registries":["registry.roger.dal.moz.com:5000"],"dns":["10.40.0.2"]}' | sudo tee /etc/docker/daemon.json
+# echo '{}' | sudo tee /etc/docker/daemon.json
 
 sudo usermod -aG docker $(whoami)
 sudo systemctl enable docker
 
 curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o docker-compose
-sudo cp docker-compose /usr/local/bin/docker-compose
+sudo mv docker-compose /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
